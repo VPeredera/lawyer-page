e8b7e84c5f144ff25ad98319417d5ae6d247e23e
VPeredera <vladislavperedera@gmail.com>
2023-07-05 00:20:40 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import logo from './components/lawyer-logo.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <img src={logo} className="logo" alt="logo"/>
+          <p>Twvdnwwnven</p>
       </header>
-    </div>
+    </main>
   );
 }
 
